b93103f1a48417b217918c834bc5f3d89b2a91c4
quami <collinskwame2000@gmail.com>
2016-06-18 20:55:12 +0000

    ajax

--- js/ajax.js
+++ js/ajax.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
 		$.ajax({
 			type: 'POST',
 			cache:false,
-			url: 'http://http://b3f44fd6.ngrok.io/pvs/php/authenticator.php',
+			url: 'http://b3f44fd6.ngrok.io/pvs/php/authenticator.php',
 			data: {"product_name":product_name,"fda_id":fda_id},
 
 			success: function(res) {
